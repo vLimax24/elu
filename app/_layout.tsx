b233b761184e0b6dbd9dc34f03f2a83fa69204fc
Limax <limax.workspace@gmail.com>
2024-09-13 17:56:27 +0200

    fix: add provider wrapper for auth and convex

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,8 @@ import "@/global.css";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Theme, ThemeProvider } from "@react-navigation/native";
-import { SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Stack, Slot } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
 import { Platform } from "react-native";
@@ -11,6 +12,10 @@ import { useColorScheme } from "@/lib/useColorScheme";
 import { PortalHost } from "@rn-primitives/portal";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { setAndroidNavigationBar } from "@/lib/android-navigation-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ClerkProvider, useAuth, ClerkLoaded } from "@clerk/clerk-expo";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
 
 const LIGHT_THEME: Theme = {
   dark: false,
@@ -63,19 +68,43 @@ export default function RootLayout() {
     return null;
   }
 
+  const convex = new ConvexReactClient(
+    process.env.EXPO_PUBLIC_CONVEX_URL as string
+  );
+
+  const tokenCache = {
+    async getToken(key: string) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+
   return (
-    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            title: "Starter Base",
-            headerRight: () => <ThemeToggle />,
-          }}
-        />
-      </Stack>
-      <PortalHost />
-    </ThemeProvider>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+      tokenCache={tokenCache}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <ClerkLoaded>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+              <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+              <Slot initialRouteName="/welcome" />
+              <PortalHost />
+            </ThemeProvider>
+          </GestureHandlerRootView>
+        </ClerkLoaded>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 }
