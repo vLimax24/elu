33fac3eec19741d575870bd54061aa52f0aa6cf6
Limax <limax.workspace@gmail.com>
2024-10-05 09:52:23 +0200

    chore: fix linting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,112 +1,117 @@
-import "@/global.css";
+import '@/global.css';
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Theme, ThemeProvider } from "@react-navigation/native";
-import { SplashScreen, Slot } from "expo-router";
-import * as SecureStore from "expo-secure-store";
-import { StatusBar } from "expo-status-bar";
-import * as React from "react";
-import { Platform } from "react-native";
-import { NAV_THEME } from "@/lib/constants";
-import { useColorScheme } from "@/lib/useColorScheme";
-import { PortalHost } from "@rn-primitives/portal";
-import { View } from "react-native";
-import { setAndroidNavigationBar } from "@/lib/android-navigation-bar";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { ClerkProvider, useAuth, ClerkLoaded } from "@clerk/clerk-expo";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Theme, ThemeProvider } from '@react-navigation/native';
+import { SplashScreen, Slot } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform, View } from 'react-native';
+import { NAV_THEME } from '@/lib/constants';
+import { useColorScheme } from '@/lib/useColorScheme';
+import { PortalHost } from '@rn-primitives/portal';
+import { setAndroidNavigationBar } from '@/lib/android-navigation-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ClerkProvider, useAuth, ClerkLoaded } from '@clerk/clerk-expo';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ConvexReactClient } from 'convex/react';
 
 const LIGHT_THEME: Theme = {
-  dark: false,
-  colors: NAV_THEME.light,
+    dark: false,
+    colors: NAV_THEME.light,
 };
 const DARK_THEME: Theme = {
-  dark: true,
-  colors: NAV_THEME.dark,
+    dark: true,
+    colors: NAV_THEME.dark,
 };
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
+} from 'expo-router';
 
 // Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
-  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+    const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+    const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
-  React.useEffect(() => {
-    (async () => {
-      const theme = await AsyncStorage.getItem("theme");
-      if (Platform.OS === "web") {
-        // Adds the background color to the html element to prevent white background on overscroll.
-        document.documentElement.classList.add("bg-background");
-      }
-      if (!theme) {
-        AsyncStorage.setItem("theme", colorScheme);
-        setIsColorSchemeLoaded(true);
-        return;
-      }
-      const colorTheme = theme === "dark" ? "dark" : "light";
-      if (colorTheme !== colorScheme) {
-        setColorScheme(colorTheme);
-        setAndroidNavigationBar(colorTheme);
-        setIsColorSchemeLoaded(true);
-        return;
-      }
-      setAndroidNavigationBar(colorTheme);
-      setIsColorSchemeLoaded(true);
-    })().finally(() => {
-      SplashScreen.hideAsync();
-    });
-  }, []);
+    React.useEffect(() => {
+        (async () => {
+            const theme = await AsyncStorage.getItem('theme');
+            if (Platform.OS === 'web') {
+                // Adds the background color to the html element to prevent white background on overscroll.
+                document.documentElement.classList.add('bg-background');
+            }
+            if (!theme) {
+                AsyncStorage.setItem('theme', colorScheme);
+                setIsColorSchemeLoaded(true);
+                return;
+            }
+            const colorTheme = theme === 'dark' ? 'dark' : 'light';
+            if (colorTheme !== colorScheme) {
+                setColorScheme(colorTheme);
+                setAndroidNavigationBar(colorTheme);
+                setIsColorSchemeLoaded(true);
+                return;
+            }
+            setAndroidNavigationBar(colorTheme);
+            setIsColorSchemeLoaded(true);
+        })().finally(() => {
+            SplashScreen.hideAsync();
+        });
+    }, []);
 
-  if (!isColorSchemeLoaded) {
-    return null;
-  }
-
-  const convex = new ConvexReactClient(
-    process.env.EXPO_PUBLIC_CONVEX_URL as string
-  );
-
-  const tokenCache = {
-    async getToken(key: string) {
-      try {
-        return SecureStore.getItemAsync(key);
-      } catch (err) {
+    if (!isColorSchemeLoaded) {
         return null;
-      }
-    },
-    async saveToken(key: string, value: string) {
-      try {
-        return SecureStore.setItemAsync(key, value);
-      } catch (err) {
-        return;
-      }
-    },
-  };
+    }
 
-  return (
-    <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
-      tokenCache={tokenCache}
-    >
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <ClerkLoaded>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-              <View className="flex-1 bg-backgroundColor">
-                <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
-                <Slot initialRouteName="/welcome" />
-                <PortalHost />
-              </View>
-            </ThemeProvider>
-          </GestureHandlerRootView>
-        </ClerkLoaded>
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
-  );
+    const convex = new ConvexReactClient(
+        process.env.EXPO_PUBLIC_CONVEX_URL as string
+    );
+
+    const tokenCache = {
+        async getToken(key: string) {
+            try {
+                return SecureStore.getItemAsync(key);
+            } catch (e) {
+                return null;
+            }
+        },
+        async saveToken(key: string, value: string) {
+            try {
+                return SecureStore.setItemAsync(key, value);
+            } catch (err) {
+                return;
+            }
+        },
+    };
+
+    return (
+        <ClerkProvider
+            publishableKey={
+                process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string
+            }
+            tokenCache={tokenCache}
+        >
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <ClerkLoaded>
+                    <GestureHandlerRootView style={{ flex: 1 }}>
+                        <ThemeProvider
+                            value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}
+                        >
+                            <View className="flex-1 bg-backgroundColor">
+                                <StatusBar
+                                    style={isDarkColorScheme ? 'light' : 'dark'}
+                                />
+                                <Slot initialRouteName="/welcome" />
+                                <PortalHost />
+                            </View>
+                        </ThemeProvider>
+                    </GestureHandlerRootView>
+                </ClerkLoaded>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
 }
