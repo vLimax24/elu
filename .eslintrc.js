d6595531e83d781909df6996f554d707beb92d6f
Limax <limax.workspace@gmail.com>
2024-10-05 09:46:31 +0200

    infra: add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-native/all',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended', // This makes sure Prettier runs as an ESLint rule
+    ],
+    plugins: [
+        'react',
+        'react-native',
+        '@typescript-eslint',
+        'import',
+        'jsx-a11y',
+    ],
+    env: {
+        'react-native/react-native': true,
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        // General rules
+        'no-unused-vars': 'warn',
+        'no-console': 'warn',
+
+        'prettier/prettier': 'error', // Treat Prettier issues as ESLint errors
+
+        // TypeScript specific rules
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+        // React specific rules
+        'react/prop-types': 'off',
+        'react/jsx-boolean-value': ['error', 'always'],
+        'react/jsx-no-bind': [
+            'error',
+            {
+                ignoreRefs: true,
+            },
+        ],
+
+        // React Native specific rules
+        'react-native/no-unused-styles': 'warn',
+        'react-native/no-inline-styles': 'off', // You can enable this if you want to strictly avoid inline styles
+        'react-native/no-color-literals': 'off',
+
+        // Accessibility rules for React Native
+        'jsx-a11y/accessible-emoji': 'warn',
+    },
+};
